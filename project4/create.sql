5fb9c19eb4937d201e82930a9485672fd6f4e80d
Brian Bugert <coolbots7@gmail.com>
2019-03-26 22:58:26 -0700

    add sql scripts to create and populate the art database

---
+++ project4/create.sql
@@ -0,0 +1,78 @@
+DROP DATABASE IF EXISTS cpsc332_project4;
+CREATE DATABASE cpsc332_project4;
+USE cpsc332_project4;
+
+CREATE TABLE t_ArtStyle (
+	ArtStyleID INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR(100),
+	PRIMARY KEY (ArtStyleID),
+	UNIQUE(Name)
+);
+
+CREATE TABLE t_ArtType (
+	ArtTypeID INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR(100),
+	PRIMARY KEY(ArtTypeID),
+	UNIQUE(Name)
+);
+
+CREATE TABLE t_Location (
+	LocationID INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR(100),
+	PRIMARY KEY (LocationID),
+	UNIQUE(Name)
+);
+
+CREATE TABLE t_Artist (
+	ArtistID INT NOT NULL AUTO_INCREMENT,
+	FirstName VARCHAR(100) NOT NULL,
+	LastName VARCHAR(100) NOT NULL,
+	PhoneNumber VARCHAR(10) NOT NULL,
+	Address VARCHAR(100) NOT NULL,
+	BirthPlace VARCHAR(100) NOT NULL,
+	Age INT NOT NULL,
+	ArtStyleID INT NOT NULL,
+	PRIMARY KEY (ArtistID),
+	UNIQUE(FirstName, LastName),
+	FOREIGN KEY (ArtStyleID) REFERENCES t_ArtStyle(ArtStyleID)
+);
+
+CREATE TABLE t_Artwork (
+	ArtworkID INT NOT NULL AUTO_INCREMENT,
+	ArtistID INT NOT NULL,
+	Title VARCHAR(100) NOT NULL,
+	ArtStyleID INT NOT NULL,
+	ArtTypeID INT NOT NULL,
+	CreatedOn DATE,
+	PurchasedOn DATE NOT NULL,
+	Price FLOAT NOT NULL,
+	LocationID INT NOT NULL,
+	PRIMARY KEY (ArtworkID),
+	UNIQUE(Title),
+	FOREIGN KEY (ArtistID) REFERENCES t_Artist(ArtistID),
+	FOREIGN KEY (ArtStyleID) REFERENCES t_ArtStyle(ArtStyleID),
+	FOREIGN KEY (ArtTypeID) REFERENCES t_ArtType(ArtTypeID),
+	FOREIGN KEY (LocationID) REFERENCES t_Location(LocationID)
+);
+
+CREATE TABLE t_Customer (
+	CustomerID INT NOT NULL AUTO_INCREMENT,
+	PhoneNumber VARCHAR(10) NOT NULL,
+	PreferredArtStyleID INT NOT NULL,
+	PRIMARY KEY (CustomerID),
+	UNIQUE (PhoneNumber),
+	FOREIGN KEY (PreferredArtStyleID) REFERENCES t_ArtStyle(ArtStyleID)
+);
+
+CREATE TABLE t_ArtShow (
+	ArtShowID INT NOT NULL AUTO_INCREMENT,
+	ArtistID INT NOT NULL,
+	ShowDate DateTime NOT NULL,
+	LocationID INT NOT NULL,
+	ContactName VARCHAR(100) NOT NULL,
+	ContactPhoneNumber VARCHAR(10) NOT NULL,
+	PRIMARY KEY (ArtShowID),
+	UNIQUE (ArtistID, ShowDate),
+	FOREIGN KEY (ArtistID) REFERENCES t_Artist(ArtistID),
+	FOREIGN KEY (LocationID) REFERENCES t_Location(LocationID)
+);
---
+++ project4/populate.sql
@@ -0,0 +1,57 @@
+USE cpsc332_project4;
+
+INSERT INTO t_ArtStyle
+     (Name)
+VALUES
+     ('Cubism'),
+     ('Florentine'),
+     ('Post-Impressionism'),
+     ('Abstract Expressionism');
+
+INSERT INTO t_ArtType
+     (Name)
+VALUES
+     ('Painting'),
+     ('Illustration'),
+     ('Photograph'),
+     ('Sculpture');
+
+INSERT INTO t_Location
+     (Name)
+VALUES
+     ('Hollywood, Los Angeles'),
+     ('New York, New York'),
+     ('Paris, France'),
+     ('Austin, Texas');
+
+INSERT INTO t_Artist
+     (FirstName, LastName, PhoneNumber, Address, BirthPlace, Age, ArtStyleID)
+VALUES
+     ('Pablo', 'Picasso', '8885551234', 'Mougins, Spain', 'Malaga, Spain', 91, 1),
+     ('Vincent', 'van Gogh', '8082345555', 'Auvers-sur-Oise, France', 'Zundert, Netherlands', 37, 3),
+     ('Leonardo', 'da Vinci', '9095559876', 'Chateau du Clos Luce, Amboise, France', 'Anchiano, Italy', 67, 2),
+     ('Jackson', 'Pollock', '5558884567', 'Springs, NY', 'Cody, WY', 44, 4);
+
+INSERT INTO t_Artwork
+     (ArtistID, Title, ArtStyleID, ArtTypeID, CreatedOn, PurchasedOn, Price, LocationID)
+VALUES
+     (1, 'Guernica', 1, 1, '1937-06-01', '1974-02-28', 1000000.01, 1),
+     (2, 'The Starry Night', 3, 1, '1889-06-01', '1941-01-01', 50000000.00, 2),
+     (3, 'Mona Lisa', 2, 1, '1503-01-01', '1797-01-01', 100.00, 3),
+     (4, 'Mural', 4, 1, '1943-01-01', '2015-01-01', 150000000.00, 4);
+
+INSERT INTO t_Customer
+     (PhoneNumber, PreferredArtStyleID)
+VALUES
+     ('8881234567', 1),
+     ('8089871234', 2),
+     ('9094561234', 3),
+     ('8887654321', 4);
+
+INSERT INTO t_ArtShow
+     (ArtistID, ShowDate, LocationID, ContactPhoneNumber, ContactName)
+VALUES
+     (1, '2019-04-01', 1, '1111111111', 'John Doe'),
+     (2, '2020-06-23', 2, '2222222222', 'Jane Doe'),
+     (3, '2019-11-16', 3, '3333333333', 'John Hancock'),
+     (4, '2021-07-26', 4, '4444444444', 'Alexander Hamilton');
